22abb382073ec0b2491b53b0ec3345e2d86cd53f
Richard <richerd.rdc@gmail.com>
2021-09-05 17:17:24 -0700

    Rewrite hook-component in TypeScript

--- src/hook-component/index.ts
+++
@@ -1,8 +0,0 @@
-import { dependencies } from "./hook-component";
-import { prepareHooks, resetHooks } from "../work-in-progress-hook";
-import Roact from "@rbxts/roact";
-
-// Provide dependencies
-[dependencies.Roact, dependencies.prepareHooks, dependencies.resetHooks] = [Roact, prepareHooks, resetHooks];
-
-export { hookComponent, HookedComponent } from "./hook-component";
---
+++ src/with-hooks/index.ts
@@ -0,0 +1,2 @@
+export type { ComponentWithHooks, ComponentWithHooksConstructor } from "./component-with-hooks";
+export { withHooks, withHooksPure } from "./with-hooks";
