d1d808aa67a09eda826e825f669c2f10f636c6b2
Richard <richerd.rdc@gmail.com>
2021-09-05 17:22:15 -0700

    Rewrite hook-component in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,13 @@
-import { hookComponent } from "./hook-component";
+import { ComponentWithHooksConstructor, withHooks, withHooksPure } from "./with-hooks";
 import Roact from "@rbxts/roact";
-import type { HookedComponentConstructor } from "./types";
 
-export type {
-	FC,
-	HookedComponentConstructor,
-	RoactContext,
-	Dispatch,
-	DispatchWithoutAction,
-	Reducer,
-	ReducerWithoutAction,
-} from "./types";
+export type { RoactContext, Dispatch, DispatchWithoutAction, Reducer, ReducerWithoutAction } from "./types";
 
 export * from "./hooks";
 
 /**
  * `hooked` is a [higher-order component](https://reactjs.org/docs/higher-order-components.html) that turns your
- * Function Component into a [Roact Component](https://roblox.github.io/roact/guide/components/).
+ * Function Component into a [class component](https://roblox.github.io/roact/guide/components/).
  *
  * `hooked` allows you to hook into the Component's lifecycle through Hooks.
  *
@@ -27,18 +18,15 @@ export * from "./hooks";
  *   },
  * );
  *
- * @param render - A Function Component
- * @returns A Component class
- *
  * @see https://reactjs.org/docs/hooks-intro.html
  */
-export function hooked<P = {}>(render: Roact.FunctionComponent<P>): HookedComponentConstructor<P> {
-	return hookComponent<P>(render, Roact.Component);
+export function hooked<P = {}>(functionComponent: Roact.FunctionComponent<P>): ComponentWithHooksConstructor<P> {
+	return withHooks<P>(functionComponent);
 }
 
 /**
  * `pure` is a [higher-order component](https://reactjs.org/docs/higher-order-components.html) that turns your
- * Function Component into a [Roact PureComponent](https://roblox.github.io/roact/performance/reduce-reconciliation/#purecomponent).
+ * Function Component into a [PureComponent](https://roblox.github.io/roact/performance/reduce-reconciliation/#purecomponent).
  *
  * If your function component wrapped in `pure` has a {@link useState}, {@link useReducer} or {@link useContext} Hook
  * in its implementation, it will still rerender when state or context changes.
@@ -50,12 +38,9 @@ export function hooked<P = {}>(render: Roact.FunctionComponent<P>): HookedCompon
  *   },
  * );
  *
- * @param render - A Function Component
- * @returns A PureComponent class
- *
  * @see https://reactjs.org/docs/react-api.html
  * @see https://roblox.github.io/roact/performance/reduce-reconciliation/
  */
-export function pure<P = {}>(render: Roact.FunctionComponent<P>): HookedComponentConstructor<P> {
-	return hookComponent<P>(render, Roact.PureComponent);
+export function pure<P = {}>(functionComponent: Roact.FunctionComponent<P>): ComponentWithHooksConstructor<P> {
+	return withHooksPure<P>(functionComponent);
 }
