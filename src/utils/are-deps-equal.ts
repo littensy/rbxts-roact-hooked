b292519d7dc47181040d56fea0f0fbbb2e863b47
Richard <0866playback@gmail.com>
2022-02-18 13:12:52 -0800

    Fix `areDepsEqual` returning true when nextDeps is empty

--- src/utils/are-deps-equal.ts
+++ src/utils/are-deps-equal.ts
@@ -1,6 +1,19 @@
 import type { DependencyList } from "../types";
 
 export function areDepsEqual(nextDeps: DependencyList, prevDeps?: DependencyList) {
-	if (!prevDeps) return false;
-	return (nextDeps as Array<defined>).every((value, index) => prevDeps[index] === value);
+	if (prevDeps === undefined) {
+		return false;
+	}
+
+	if (nextDeps.size() !== prevDeps.size()) {
+		return false;
+	}
+
+	for (let i = 0; i < nextDeps.size(); i++) {
+		if (nextDeps[i] === prevDeps[i]) {
+			continue;
+		}
+		return false;
+	}
+	return true;
 }
