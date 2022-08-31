33384a0d559e198be0b63183bd950c3194271a91
Richard <richerd.rdc@gmail.com>
2022-08-30 21:27:56 -0700

    Update Roact.lua

--- src/Roact.lua
+++ src/Roact.lua
@@ -1,5 +1,9 @@
-if script:FindFirstAncestor("node_modules") then
-	return require(script:FindFirstAncestor("node_modules").roact.src)
+local modules = script:FindFirstAncestor("node_modules")
+
+if modules:FindFirstChild("roact") then
+	return require(modules.roact.src)
+elseif modules:FindFirstChild("@rbxts") then
+	return require(modules["@rbxts"].roact.src)
 elseif script.Parent.Parent:FindFirstChild("Roact") then
 	return require(script.Parent.Parent.Roact)
 else
